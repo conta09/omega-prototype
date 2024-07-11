921726cf23fb2d53536078a57582cd9173b45912
conta09 <mucyoconta8@gmail.com>
2024-07-11 14:21:26 +0200

    to dashboard

--- app/components/AuthComponent.js
+++ app/components/AuthComponent.js
@@ -49,6 +49,7 @@ const AuthComponent = () => {
       if (res.ok) {
         const form = e.target;
         form.reset();
+        router.push("/dashboard");
       } else {
         console.log("User registration failed.");
       }
